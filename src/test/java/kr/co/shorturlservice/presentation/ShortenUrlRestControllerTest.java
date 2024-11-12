ce455a6eb3eebe6a5aab0735bb53751c77a58811
erinleeme <gusdk1793@gmail.com>
2024-11-12 20:23:42 +0900

    test:프레젠테이션 테스트 코드 작성

--- src/test/java/kr/co/shorturlservice/presentation/ShortenUrlRestControllerTest.java
+++ src/test/java/kr/co/shorturlservice/presentation/ShortenUrlRestControllerTest.java
@@ -1,6 +1,8 @@
 package kr.co.shorturlservice.presentation;
 
 import kr.co.shorturlservice.application.SimpleShortenUrlService;
+import kr.co.shorturlservice.domain.NotFoundShortenUrlException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,8 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = ShortenUrlRestController.class)
 public class ShortenUrlRestControllerTest {
@@ -23,6 +24,7 @@ public class ShortenUrlRestControllerTest {
     private MockMvc mockmvc;
 
     @Test
+    @DisplayName("원래의 URL로 리다이렉트 되어야한다.")
     void redirectShortenUrl_Success() throws Exception {
         String originalUrl = "https://www.naver.com";
 
@@ -31,6 +33,17 @@ public class ShortenUrlRestControllerTest {
         mockmvc.perform(get("/any-key"))
                 .andExpect(status().isMovedPermanently())
                 .andExpect(header().string("Location", originalUrl))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("없는 단축 URL인 경우에는 에러를 반환한다.")
+    void redirectShortenUrl_Fail() throws Exception {
+        when(simpleShortenUrlService.getOriginalUrlByShortenUrlKey(any())).thenThrow(NotFoundShortenUrlException.class);
+
+        mockmvc.perform(get("/any-key"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("단축 url을 찾지 못했습니다."))
                 .andDo(print());
     }
 }
