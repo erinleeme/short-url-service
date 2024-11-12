6d8f24b3f6ace0d532dfc5b4a2db1d9e6b057226
erinleeme <gusdk1793@gmail.com>
2024-11-12 15:21:47 +0900

    refactor:어노테이션 추가

--- src/main/java/kr/co/shorturlservice/presentation/ShortenUrlRestController.java
+++ src/main/java/kr/co/shorturlservice/presentation/ShortenUrlRestController.java
@@ -4,11 +4,9 @@ import jakarta.validation.Valid;
 import kr.co.shorturlservice.application.SimpleShortenUrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class ShortenUrlRestController {
 
     private final SimpleShortenUrlService simpleShortenUrlService;
